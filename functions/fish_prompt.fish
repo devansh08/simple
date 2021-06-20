837b62360c88e9e3570ef46e70e3561cf4b2da56
Devansh Sharma <devansh.sharma@gmail.com>
2021-06-20 17:37:24 +0530

    Code formatting/cleanup

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,85 +1,84 @@
 # fish theme: simple
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD 2> /dev/null | sed -e "s|^refs/heads/||")
 end
 
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
 # change color depending on the user.
 function _user_host
-  if [ (id -u) = "0" ];
-    echo -n (set_color -o red)
-  else
-    echo -n (set_color -o blue)
-  end
-  echo -n (hostname|cut -d . -f 1)ˇ$USER (set color normal)
+    if [ (id -u) = 0 ]
+        echo -n (set_color -o red)
+    else
+        echo -n (set_color -o blue)
+    end
+    echo -n (hostname|cut -d . -f 1)$USER (set color normal)
 end
 
 function _short_cwd
-  set -l path_splits (string split / (prompt_pwd))
+    set path_splits (string split / (prompt_pwd))
 
-  set -l final_path ""
-  set -l i 0
+    set final_path ""
+    set i 0
 
-  for d in (string split / (prompt_pwd))
-    set -l d_char (echo $d | grep -oE "^[^a-zA-Z0-9~]*[a-zA-Z0-9~]{1}")
+    for d in (string split / (prompt_pwd))
+        set d_char (echo $d | grep -oE "^[^a-zA-Z0-9~]*[a-zA-Z0-9~]{1}")
 
-    if test $i = (math (string join \n $path_splits | wc -l) - 1) -a "$d" != ""
-      set -l d_char (basename (prompt_pwd))
+        if test $i = (math (string join \n $path_splits | wc -l) - 1) -a "$d" != ""
+            set d_char (basename (prompt_pwd))
+        end
+
+        if test $i = 0
+            set final_path "$d_char"
+        else
+            set final_path "$final_path/$d_char"
+        end
+
+        set i (math $i + 1)
     end
 
-    if test $i = 0
-      set -l final_path "$d_char"
-    else
-      set -l final_path "$final_path/$d_char"
-    end
-
-    set -l i (math $i + 1)
-  end
-
-  echo $final_path
+    echo $final_path
 end
 
 function fish_prompt
-  set -l last_status $status
-  set fish_greeting
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -l normal (set_color normal)
+    set -l last_status $status
+    set fish_greeting
+    set -l cyan (set_color -o cyan)
+    set -l yellow (set_color -o yellow)
+    set -l red (set_color -o red)
+    set -l blue (set_color -o blue)
+    set -l green (set_color -o green)
+    set -l normal (set_color normal)
 
-  set -l cwd $cyan(_short_cwd)
+    set -l cwd $cyan(_short_cwd)
 
-  # output the prompt, left to right:
-  # display 'user@host:'
-  echo -n -s $green (whoami) $dark_green @ $green (hostname|cut -d . -f 1) ": "
+    # output the prompt, left to right:
+    # display "user@host:"
+    echo -n -s $green (whoami) $dark_green @ $green (hostname|cut -d . -f 1) ": "
 
-  # display the current directory name:
-  echo -n -s $cwd $normal
+    # display the current directory name:
+    echo -n -s $cwd $normal
 
-  # show git branch and dirty state, if applicable:
-  if [ (_git_branch_name) ]
-    set -l git_branch '[' (_git_branch_name) ']'
+    # show git branch and dirty state, if applicable:
+    if [ (_git_branch_name) ]
+        set -l git_branch "[" (_git_branch_name) "]"
 
-    if [ (_is_git_dirty) ]
-      set git_info $red $git_branch "×"
-    else
-      set git_info $green $git_branch
+        if [ (_is_git_dirty) ]
+            set git_info $red $git_branch ""
+        else
+            set git_info $green $git_branch
+        end
+        echo -n -s " " $git_info $normal
     end
-    echo -n -s ' ' $git_info $normal
-  end
 
-  # terminate with a nice prompt char:
-  if test $last_status -ne 0
-    echo -n -s $red
-  else
-    echo -n -s $normal
-  end
-  echo -n -s " 〉" $normal
-
+    # terminate with a nice prompt char:
+    if test $last_status -ne 0
+        echo -n -s $red
+    else
+        echo -n -s $normal
+    end
+    echo -n -s " 〉" $normal
 end
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,26 +1,26 @@
 # fish theme: simple
 
 function fish_right_prompt
-  set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l red (set_color -o red)
-  set -l normal (set_color normal)
-  set -l bg_processes (jobs -p | wc -l)
+    set -l last_status $status
+    set -l cyan (set_color -o cyan)
+    set -l red (set_color -o red)
+    set -l normal (set_color normal)
+    set -l bg_processes (jobs -p | wc -l)
 
-  # print number of bg processes running if nonzero
-  if test $bg_processes -ne 0
-    set_color cyan
-    echo -n -s "$bg_processes&"
-    set_color normal
-  end
+    # print number of bg processes running if nonzero
+    if test $bg_processes -ne 0
+        set_color cyan
+        echo -n -s "$bg_processes&"
+        set_color normal
+    end
 
-  # print last exit code if nonzero
-  if test $last_status -ne 0
-    set_color red
-    echo -n -s " -$last_status-"
-    set_color normal
-  end
+    # print last exit code if nonzero
+    if test $last_status -ne 0
+        set_color red
+        echo -n -s " -$last_status-"
+        set_color normal
+    end
 
-  # display the timestamp on the utmost right
-  echo -n -s $normal ' [' (date +%H:%M:%S) ']'
+    # display the timestamp on the utmost right
+    echo -n -s $normal " [" (date +%H:%M:%S) "]"
 end
