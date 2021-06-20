b149cf0685e9cfb7aec2da0a60a7c620be84af01
Devansh Sharma <devansh.sharma@gmail.com>
2021-06-20 17:28:52 +0530

    Added bg process count to right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -5,6 +5,14 @@ function fish_right_prompt
   set -l cyan (set_color -o cyan)
   set -l red (set_color -o red)
   set -l normal (set_color normal)
+  set -l bg_processes (jobs -p | wc -l)
+
+  # print number of bg processes running if nonzero
+  if test $bg_processes -ne 0
+    set_color cyan
+    echo -n -s "$bg_processes&"
+    set_color normal
+  end
 
   # print last exit code if nonzero
   if test $last_status -ne 0
